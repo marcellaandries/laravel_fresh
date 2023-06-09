f3efdb5dc29b0b8306e8148fe6f4efa1fbc3d091
marcellaandries <marcella.andries11@gmail.com>
2023-06-09 10:39:50 +0700

    add controller

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+{
+    //new code
+    public function home(){
+        
+    }
+}
