d75b129c01dc69ba2ac5d1bc99caa74bf74acfbe
marcellaandries <marcella.andries11@gmail.com>
2023-06-09 10:06:14 +0700

    add new route formula by URL

--- resources/views/about_page.blade.php
+++ resources/views/about_page.blade.php
@@ -1,7 +1,7 @@
-<h1>Home Page </h1>   
+<h1>About Page </h1>   
 <ul>
-    <li><a href="{{ route('front_home') }}">Home</li>
-    <li><a href="{{ route('front_about') }}">About</li>
+    <li><a href="{{ url('/') }}">Home</li>
+    <li><a href="{{ url('/about') }}">About</li>
 </ul>   
 <p>
     About page description
