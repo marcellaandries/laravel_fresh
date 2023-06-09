73f42d99ae47c6897b29442ef2315afbe5516bf5
marcellaandries <marcella.andries11@gmail.com>
2023-06-09 08:09:24 +0700

    add dev notes

--- README.md
+++ README.md
@@ -1,17 +1,3 @@
-composer create-project laravel/laravel laravel_fresh
-
-php artisan serve -> to npm run serve
-
-php artisan -> too see cmd
-composer update -> to npm i
-
-to clear cache:
-php artisan cache:clear
-php artisan view:clear
-php artisan route:clear
-php artisan clear-compiled
-php artisan config:cache
-
 <p align="center"><a href="https://laravel.com" target="_blank"><img src="https://raw.githubusercontent.com/laravel/art/master/logo-lockup/5%20SVG/2%20CMYK/1%20Full%20Color/laravel-logolockup-cmyk-red.svg" width="400"></a></p>
 
 <p align="center">
--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,35 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return redirect('home');
 });
-
-Route::get('/about/marcella', function () {
-    return view('about_page');
-});
-
-//url, page
-Route::view('/about/cella', 'about_page');
 
 Route::get('/about/{username}/{age}', function ($username, $age) {
     return view('about_page',['un'=>$username, 'age'=>$age]);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// lesson archive
+//url, page
+
+// Route::get('/about/marcella', function () {
+//     return view('about_page');
+// });
+
+// Route::view('/about/cella', 'about_page');
+
+// Route::get('/about/{username}/{age}', function ($username, $age) {
+//     return view('about_page',['un'=>$username, 'age'=>$age]);
+// });
