5092bcde9a7c8f95ce67df760fe16bbd2cb95e0f
marcellaandries <marcella.andries11@gmail.com>
2023-06-10 15:41:53 +0700

    echo data in view from controller

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -9,7 +9,18 @@ class StudentController extends Controller
     
     //new code
     public function index(){
-        return view('welcome');
+        $data ['student_name'] = [
+            'name' => 'Marcella Andries',
+            'email' => 'marcella@gmail.com',
+            'phone' => '021'
+        ];
+
+        // dd($data);
+        foreach($data['student_name'] as $value) {
+            echo $value.'-';
+        }
+
+        return view('welcome', $data);
     }
 
     public function home(){
