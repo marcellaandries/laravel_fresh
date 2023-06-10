5419484a6febb1d5c111686797ca2dfccfbace48
marcellaandries <marcella.andries11@gmail.com>
2023-06-10 16:42:12 +0700

    declaration and if

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,19 @@
 <h1>Test</h1>
 
-    @foreach($student_name as $value) 
-        {{ $value }} <br>
-    @endforeach
+<!-- @foreach($student_name as $value) 
+    {{ $value }} <br>
+@endforeach
 
-    @for($i=1;$i<=10;$i++)
-        {{ $i.' ' }}
-    @endfor
+@for($i=1;$i<=10;$i++)
+    {{ $i.' ' }}
+@endfor -->
+
+@php
+$i=10
+@endphp
+
+@if($i==10)
+This is fine
+@else
+This is not fine
+@endif
